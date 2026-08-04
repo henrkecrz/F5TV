e66b2c4625b1ab0921920a5f0ee59313bde1e71d
henrikecrz <147286609+henrikecrz@users.noreply.github.com>
2026-08-04 11:18:37 -0300

    style: replace remaining homepage black areas

--- wp-content/themes/f5tv-theme/functions.php
+++ wp-content/themes/f5tv-theme/functions.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('F5TV_THEME_VERSION', '1.0.7');
+define('F5TV_THEME_VERSION', '1.0.8');
 define('F5TV_THEME_DIR', get_template_directory());
 define('F5TV_THEME_URI', get_template_directory_uri());
 define('F5TV_ASSETS_DIR', F5TV_THEME_DIR . '/assets');
